9ae0042b787fe1eae315fc9b7d8170ecc45caabb
Agraj Mangal <agraj.mng@gmail.com>
2020-03-23 19:38:05 +1100

    test os commands

---
+++ interfaces/fileprinter.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+
+	fileName := os.Args[1]
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("Error in opening file ", err)
+		os.Exit(1)
+	}
+	io.Copy(os.Stdout, file)
+}
